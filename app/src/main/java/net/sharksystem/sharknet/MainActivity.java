d7380d18a6bd9f9fc4b579c00a6bd8c8d4d32198
Michael Schwarz <schwarzm90@gmail.com>
2016-08-05 11:19:47 +0200

    Init with SharkNet-Api-Android

---
+++ app/src/main/java/net/sharksystem/sharknet/MainActivity.java
@@ -0,0 +1,15 @@
+package net.sharksystem.sharknet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.sharksystem.android.peer.SharkServiceController;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
