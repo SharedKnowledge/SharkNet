843986a25fd641583025ea1207b07615ca7c099b
Dustin <bosche@hotmail.de>
2017-09-28 22:45:51 +0200

    Further work on the annotations of messages

--- app/src/main/java/net/sharksystem/sharknet/chat/ChatAnnotationPeerActivity.java
+++ app/src/main/java/net/sharksystem/sharknet/chat/ChatAnnotationPeerActivity.java
@@ -2,9 +2,7 @@ package net.sharksystem.sharknet.chat;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
 
-import net.sharksystem.api.models.Message;
 import net.sharksystem.sharknet.BaseActivity;
 import net.sharksystem.sharknet.R;
 import net.sharksystem.sharknet.SharkApp;
@@ -13,14 +11,14 @@ import net.sharksystem.sharknet.SharkApp;
  * Created by Dustin Feurich on 28.09.2017.
  */
 
-public class ChatAnnotationActivity extends BaseActivity {
+public class ChatAnnotationPeerActivity extends BaseActivity {
 
     private SharkApp application;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setLayoutResource(R.layout.chat_annotation_activity);
+        setLayoutResource(R.layout.chat_annotation_peer_activity);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         Intent intent = getIntent();
 
