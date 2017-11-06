d027d33177f3107e59cded09af9896a6087e38f3
Yannik Morhenn <yannik.morhenn@web.de>
2017-11-07 00:00:50 +0100

    Revert "moved Locator and Schnitzeljagd to api and created a DummyDao"
    This reverts commit 10d155f785bb240ed5a3b3013f693fb49d35b098.

---
+++ app/src/main/java/net/sharksystem/sharknet/schnitzeljagd/Schnitzeljagd.java
@@ -0,0 +1,40 @@
+package net.sharksystem.sharknet.schnitzeljagd;
+
+import net.sharksystem.api.models.Message;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Yannik on 04.11.2017.
+ */
+
+class Schnitzeljagd {
+    private ArrayList<Schnitzel> schnitzelList = new ArrayList<>();
+    private String description;
+
+    public Schnitzeljagd(String description){
+        this.description = description;
+    }
+    public Schnitzeljagd(ArrayList<Schnitzel> schnitzel, String description) {
+        this.schnitzelList = schnitzel;
+        this.description = description;
+    }
+
+    public ArrayList<Schnitzel> getSchnitzel() {
+        return schnitzelList;
+    }
+    public void addSchnitzel(Schnitzel schnitzel){
+        schnitzelList.add(schnitzel);
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    protected void setSchnitzel(ArrayList<Schnitzel> schnitzel){
+        this.schnitzelList = schnitzel;
+    }
+
+    protected void setDescription(String description) {
+        this.description = description;
+    }
+}
--- app/src/main/java/net/sharksystem/sharknet/schnitzeljagd/SchnitzeljagdMainActivity.java
+++ app/src/main/java/net/sharksystem/sharknet/schnitzeljagd/SchnitzeljagdMainActivity.java
@@ -7,14 +7,16 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-import net.sharksystem.api.models.Schnitzeljagd;
+import net.sharksystem.api.models.Chat;
+import net.sharksystem.api.models.Message;
 import net.sharksystem.sharknet.R;
 import net.sharksystem.sharknet.RxSingleBaseActivity;
-import net.sharksystem.api.locator.Locator;
+import net.sharksystem.sharknet.schnitzeljagd.locator.Locator;
 
 import java.util.ArrayList;
 import java.util.List;
