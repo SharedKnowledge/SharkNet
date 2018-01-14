9fe670d01361b96bed215b1a9267f9cfde3edb05
Max Oehme <maxoehme@yahoo.de>
2018-01-14 19:27:16 +0100

    added lastlocation to profile

--- app/src/main/java/net/sharksystem/sharknet/locationprofile/data/SpatialInformationImpl.java
+++ app/src/main/java/net/sharksystem/sharknet/locationprofile/data/SpatialInformationImpl.java
@@ -10,29 +10,62 @@ import net.sharkfw.knowledgeBase.spatial.SpatialInformation;
 
 public class SpatialInformationImpl implements SpatialInformation {
 
+    private double sourceToProfile;
+    private double entrenceToExit;
+    private double destinationToProfile;
+    private int weightEntrance;
+    private int weightExit;
+
+    public SpatialInformationImpl(double sourceToProfile, double entrenceToExit, double destinationToProfile, int weightEntrance, int weightExit) {
+        this.sourceToProfile = sourceToProfile;
+        this.entrenceToExit = entrenceToExit;
+        this.destinationToProfile = destinationToProfile;
+        this.weightEntrance = weightEntrance;
+        this.weightExit = weightExit;
+    }
 
     @Override
     public double getSourceToProfileDistance() {
-        return 0;
+        return this.sourceToProfile;
     }
 
     @Override
     public double getEntranceExitInProfileDistance() {
-        return 0;
+        return this.entrenceToExit;
     }
 
     @Override
     public double getDestinationToProfileDistance() {
-        return 0;
+        return this.destinationToProfile;
     }
 
     @Override
     public int getProfileEntrancePointWeight() {
-        return 0;
+        return this.weightEntrance;
     }
 
     @Override
     public int getProfileExitPointWeight() {
-        return 0;
+        return this.weightExit;
+    }
+
+    public void setSourceToProfileDistance(double sourceToProfile) {
+        this.sourceToProfile = sourceToProfile;
+    }
+
+    public void setEntranceExitInProfileDistance(double entrenceToExit) {
+        this.entrenceToExit = entrenceToExit;
+    }
+
+    public void setDestinationToProfileDistance(double destinationToProfile) {
+        this.destinationToProfile = destinationToProfile;
+    }
+
+    public void setProfileEntrancePointWeight(int weightEntrance) {
+        this.weightEntrance = weightEntrance;
+    }
+
+    public void setProfileExitPointWeight(int weightExit) {
+        this.weightExit = weightExit;
     }
 }
